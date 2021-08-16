d6cadb124370962787142fd0716fa8859e96f577
Vishal Patra <vishal.ptr@gmail.com>
2021-08-17 04:23:09 +0530

    last

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,14 +1,30 @@
 <template>
-  <div class="login">
-    <h3>Login</h3>
+  <div class="login py-5">
+    <h1 class="section-header white">HI AGAIN</h1>
+    <b-row>
+    <div class="subtitle white"><p>New to Navic?&nbsp;</p><a href="/registration"><p class="navic-color">SIGN UP</p></a></div>
+    </b-row>
     <form @submit.prevent="pressed">
-      <div class="login">
-        <input type="text" placeholder="login" v-model="email" />
+      <div class="login proformmain">
+        <b-form-group id="email" label="Email Address*" label-for="email">
+        <b-form-input
+          id="email"
+          v-model="email"
+          placeholder="Your Email Address"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group id="password" label="Password*" label-for="password">
+        <b-form-input
+         type="password"
+          id="password"
+          v-model="password"
+          placeholder="***********"
+          required
+        ></b-form-input>
+      </b-form-group>
       </div>
-      <div class="password">
-        <input type="password" placeholder="password" v-model="password" />
-      </div>
-      <button>Login</button>
+      <button class="btn-black">Login</button>
     </form>
     <div class="error" v-if="error">{{error.message}}</div>
   </div>
@@ -44,12 +60,22 @@ export default {
 
 <style  scoped>
 .login {
-  margin-top: 50px;
   display: flex;
   width: 100%;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+}
+.subtitle{
+font-family: Now;
+font-size: 18px;
+font-style: normal;
+font-weight: 500;
+line-height: 26px;
+letter-spacing: 0em;
+text-align: center;
+display:inline-flex;
+color:white;
 }
 div {
   color: inherit;
@@ -61,17 +87,72 @@ form {
   flex-direction: column;
 }
 input {
-  width: 400px;
+  width: 600px;
   padding: 30px;
-  margin: 20px;
+  margin: 0px;
   font-size: 21px;
-}
-button {
-  width: 400px;
-  height: 75px;
-  font-size: 100%;
 }
 .error {
   color: red;
+}
+.sub-text{
+font-family: Brandon Grotesque;
+font-size: 24px;
+font-style: normal;
+font-weight: 390;
+line-height: 55px;
+letter-spacing: 0em;
+text-align: center;
+}
+.proformmain{
+    background: #242728;
+    color:white;
+    align-items: center;
+    text-align: left;
+    padding: 0px;
+    font-family: NowThin;
+font-style: normal;
+font-weight: normal;
+font-size: 12px;
+line-height: 20px;
+margin:20px 120px;
+}
+.formbody{
+  margin:0px 300px;
+}
+.form-control{
+background: #242728;
+}
+#input-4{
+  height: 80px;
+}
+.formhead{
+   font-family: Brandon Grotesque;
+font-style: normal;
+font-weight: 420;
+font-size: 22px;
+line-height: 27px;
+/* or 135% */
+
+display: flex;
+align-items: center;
+text-align: center;
+}
+.custom-checkbox{
+    align-items: center;
+    align-self: center;
+font-family: NowThin;
+font-size: 16px;
+font-style: normal;
+font-weight: 400;
+line-height: 20px;
+letter-spacing: 0em;
+text-align: left;
+color:aqua;
+margin:10px;
+padding:10px;
+}
+.customform{
+    padding: 50px;
 }
 </style>
---
+++ pages/registration/index.vue
@@ -0,0 +1,175 @@
+<template>
+<b-container>
+    <h1 class="section-header white">SIGN UP</h1>
+    <div class="login">
+    <div class="subtitle white"><p>Already Part of Navic?  &nbsp;</p><a href="/login"><p class="navic-color">LOG IN</p></a> </div>
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          placeholder="Enter email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          placeholder="Enter name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Food:" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="form.food"
+          :options="foods"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
+        <b-form-checkbox-group
+          v-model="form.checked"
+          id="checkboxes-4"
+          :aria-describedby="ariaDescribedby"
+        >
+          <b-form-checkbox value="me">Check me out</b-form-checkbox>
+          <b-form-checkbox value="that">Check that out</b-form-checkbox>
+        </b-form-checkbox-group>
+      </b-form-group>
+      <b-button type="submit" variant="primary" class="btn-black" >Submit</b-button>
+    </b-form>
+      </div>
+</b-container>
+</template>
+
+<script>
+import '@firebase/auth'
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: ''
+    }
+  },
+  methods: {
+    onSubmit (event) {
+      event.preventDefault()
+      alert(JSON.stringify(this.form))
+    },
+    onReset (event) {
+      event.preventDefault()
+      // Reset our form values
+      this.form.email = ''
+      this.form.name = ''
+      this.form.message = ''
+      this.form.food = null
+      this.form.checked = []
+      // Trick to reset/clear native browser form validation state
+      this.show = false
+      this.$nextTick(() => {
+        this.show = true
+      })
+    }
+  }
+}
+</script>
+
+<style  scoped>
+.login {
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+}
+.subtitle{
+font-family: Now;
+font-size: 18px;
+font-style: normal;
+font-weight: 500;
+line-height: 26px;
+letter-spacing: 0em;
+text-align: center;
+display:inline-flex;
+color:white;
+}
+div {
+  color: inherit;
+}
+
+.error {
+  color: red;
+}
+.sub-text{
+font-family: Brandon Grotesque;
+font-size: 24px;
+font-style: normal;
+font-weight: 390;
+line-height: 55px;
+letter-spacing: 0em;
+text-align: center;
+}
+.proformmain{
+    background: #242728;
+    color:white;
+    align-items: center;
+    text-align: left;
+    padding: 0px;
+    font-family: NowThin;
+font-style: normal;
+font-weight: normal;
+font-size: 12px;
+line-height: 20px;
+margin:20px 120px;
+}
+.formbody{
+  margin:0px 300px;
+}
+.form-control{
+background: #242728;
+}
+#input-4{
+  height: 80px;
+}
+.formhead{
+   font-family: Brandon Grotesque;
+font-style: normal;
+font-weight: 420;
+font-size: 22px;
+line-height: 27px;
+/* or 135% */
+
+display: flex;
+align-items: center;
+text-align: center;
+}
+.custom-checkbox{
+    align-items: center;
+    align-self: center;
+font-family: NowThin;
+font-size: 16px;
+font-style: normal;
+font-weight: 400;
+line-height: 20px;
+letter-spacing: 0em;
+text-align: left;
+color:aqua;
+margin:10px;
+padding:10px;
+}
+.customform{
+    padding: 50px;
+}
+</style>
