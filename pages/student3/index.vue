032c1777b48ccdfc575536859df0c01d4f4d1272
Saikat Biswas <saikat1236@gmail.com>
2022-01-08 00:16:00 +0530

    student page updated

---
+++ pages/student3/index.vue
@@ -0,0 +1,120 @@
+<template>
+<b-container fluid>
+    <h1 class="section-header white pt-">MY COURSES</h1>
+            <div class="box-bg">
+        <b-row>
+            <b-col lg="6">
+                <p class="coursedate"> Started On : 02nd September 2021</p>
+                <h3 class="courseenrolled"> <b>EVOLVE</b> Go - Blockchain Unchained {{coursename}}</h3></b-col>
+            <b-col>
+                <b-row>
+                    <b-col>
+                        <h3 class="linkhead navic-color">Batch 1 Classroom</h3>
+            "<p class="courselink"> <a href="#"><b-button disabled class="white-btn"> Batch Uno</b-button></a><hr style="color:gray; background-color:gray; margin-top:2rem;">
+                    </b-col>
+                    <b-col>
+            <h3 class="linkhead navic-color">Batch 2 Classroom</h3>
+            "<p class="courselink"> <a href="#"><b-button disabled class="white-btn">Batch Dos</b-button></a><hr style="color:gray; background-color:gray; margin-top:2rem;">
+            </b-col>
+            </b-row>
+            </b-col>
+        </b-row>
+        </div>
+    <div class="box-bg">
+        <b-row>
+            <b-col lg="6">
+                <p class="coursedate"> Started On : 22nd September 2021</p>
+                <h3 class="courseenrolled"> EVOLVE - Level I {{coursename}}</h3></b-col>
+            <b-col>
+                <b-row>
+                    <b-col>
+                        <h3 class="linkhead navic-color">Batch 1 Classroom</h3>
+            "<p class="courselink"> <a href="#"><b-button disabled class="white-btn"> Batch Uno</b-button></a><hr style="color:gray; background-color:gray; margin-top:2rem;">
+                    </b-col>
+                    <b-col>
+            <h3 class="linkhead navic-color">Batch 2 Classroom</h3>
+            "<p class="courselink"> <a href="#"><b-button disabled class="white-btn">Batch Dos</b-button></a><hr style="color:gray; background-color:gray; margin-top:2rem;">
+            </b-col>
+            </b-row>
+            </b-col>
+        </b-row>
+                <b-row>
+            <b-col lg="6">
+                <p class="coursedate"> Started On : 22nd September 2021</p>
+                <h3 class="courseenrolled"> EVOLVE - Level II {{coursename}}</h3></b-col>
+            <b-col>
+                <b-row>
+                    <b-col>
+                        <h3 class="linkhead navic-color">Batch 1 Classroom</h3>
+            "<p class="courselink"> <a href="#"><b-button disabled class="white-btn"> Batch Uno</b-button></a><hr style="color:gray; background-color:gray; margin-top:2rem;">
+                    </b-col>
+                    <b-col>
+            <h3 class="linkhead navic-color">Batch 2 Classroom</h3>
+            "<p class="courselink"> <a href="#"><b-button disabled class="white-btn">Batch Dos</b-button></a><hr style="color:gray; background-color:gray; margin-top:2rem;">
+            </b-col>
+            </b-row>
+            </b-col>
+        </b-row>
+                <b-row>
+            <b-col lg="6">
+                <p class="coursedate"> Started On : 22nd September 2021</p>
+                <h3 class="courseenrolled"> EVOLVE - Level III {{coursename}}</h3></b-col>
+            <b-col>
+                <b-row>
+                    <b-col>
+                        <h3 class="linkhead navic-color">Batch 1 Classroom</h3>
+            "<p class="courselink"> <a href="https://classroom.google.com/u/0/c/MzUyMDg0MjU4NjEz"><b-button class="white-btn"> Batch Uno</b-button></a><hr style="color:gray; background-color:gray; margin-top:2rem;">
+                    </b-col>
+                    <b-col>
+            <h3 class="linkhead navic-color">Batch 2 Classroom</h3>
+            "<p class="courselink"> <a href="#"><b-button disabled class="white-btn">Batch Dos</b-button></a><hr style="color:gray; background-color:gray; margin-top:2rem;">
+            </b-col>
+            </b-row>
+            </b-col>
+        </b-row>
+    </div>
+</b-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.courseenrolled{
+    font-family: Brandon Grotesque;
+font-size: 24px;
+font-style: normal;
+font-weight: 900;
+line-height: 37px;
+letter-spacing: 0em;
+text-align: left;
+    color:white;
+}
+.coursedate{
+    font-family: NowThin;
+font-style: normal;
+font-weight: normal;
+font-size: 18px;
+line-height: 22px;
+/* identical to box height, or 122% */
+
+text-transform: capitalize;
+text-align:left;
+color: #FFFFFF;
+}
+.box-bg{
+    border-radius: 10px;
+    border: 1px solid;
+    border-color: #21E3CE;
+    align-content:center;
+    height:20%;
+    width: 90%;
+    text-align: center;
+    margin:2rem;
+    padding:2rem;
+}
+</style>
