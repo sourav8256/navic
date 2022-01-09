442a703e5d0df8cad094d6e41c47e67309725445
Saikat Biswas <saikat1236@gmail.com>
2022-01-09 17:28:50 +0530

    netlify added in form6

--- pages/evolveGo/index.vue
+++ pages/evolveGo/index.vue
@@ -30,7 +30,7 @@
           <h1 class="priceevolvebox navic-color">₹990</h1>
           <!-- <b-button href="/payment" class="btn-black"> Enroll Now </b-button> -->
 
-          <form action="/studentgo/index" method="POST" data-netlify="true">
+          <form action="/studentgo/" method="POST" data-netlify="true">
             <script
               src="https://checkout.razorpay.com/v1/checkout.js"
               data-key="rzp_test_enRiibCpGvB8WA"
